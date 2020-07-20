81019a8cc5fcdbe4b1b280862fd08972961cf089
lewman215 <lewisharvey2015@gmail.com>
2020-07-20 21:14:23 +0100

    after fist sesh

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("here is some text!");
+
+//let numberOne = 5 makes a "variable" called "number one" which holds the value "5"
+let numberOne = 5;
+let numberTwo = 3;
+let numberThree = numberOne + numberTwo;
+// "alert" makes a pop up on the web page.
+alert(numberThree);
+
+// element is being told to hold a reference to the element that has the id "myPara"
+let element = document.getElementById('myPara');
+let button = document.getElementById('colorButton');
+
+
+//a function, that is run when "button" is clicked
+button.onclick = function(){
+    element.style.color = "green";
+};
+
